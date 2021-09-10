0f35e0f994eda62936d36ea511f4eeeffb629b80
jaqmam <jaqmam@gmail.com>
2021-09-09 22:02:50 -0300

    aula inicial js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var nome = "Jonatas Jaqmam Pereira";
+var idade = 35;
+var idade2 = 10;
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(n1 * n2);
+//alert(frase.replace("Japão","Brasil"));
