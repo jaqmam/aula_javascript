564e3b93740aca9551edd4e72d752373f3b7dc34
jaqmam <jaqmam@gmail.com>
2021-09-10 13:35:28 -0300

    aula 2 javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-var nome = "Jonatas Jaqmam Pereira";
+//Dicionário
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+
+//Array de dicionários
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+alert(frutas[1].cor);
+
+//Arrays
+var lista = ["maçã","pêra","laranja"];
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));
+alert(lista[1]);
+
+
+/*var nome = "Jonatas Jaqmam Pereira";
 var idade = 35;
 var idade2 = 10;
 var n1 = 5;
@@ -12,4 +39,4 @@ console.log(frase.replace("Japão","Brasil"));
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
 console.log(n1 * n2);
-//alert(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));*/
